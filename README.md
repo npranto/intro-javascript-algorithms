533ffeed28e5c2633a7f79feb78e2ebbdcdd32f0
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-10-11 11:52:39 -0400

    adds isPalidrome challenge

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ A quick exploration and review of JavaScript mini challenges and algorithms
 + Challenges
     + [Fizz Buzz](https://github.com/npranto/intro-javascript-algorithms/blob/master/challenges/fizzBuzz.js)
     + [Harmless Ranson Note](https://github.com/npranto/intro-javascript-algorithms/blob/master/challenges/harmlessRansonNote.js)
+    + [Is Palindrome](https://github.com/npranto/intro-javascript-algorithms/blob/master/challenges/isPalindrome.js)
 
 ## Author
 * **Nazmuz Shakib Pranto** [[GitHub](https://github.com/npranto) | [LinkedIn](https://www.linkedin.com/in/npranto/)] 
---
+++ challenges/isPalindrome.js
@@ -0,0 +1,21 @@
+/**
+ * Checks to see if str is palindrome(spells the same in both front and back directions) or not
+ * @param str <String>
+ * @returns <Boolean>
+ */
+let isPalindrome = (str) => {
+    let originalWord = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => {
+            return (char.length === 1) && (char.toLowerCase().match(/[a-z]/i));
+        })
+    return originalWord.join('') === originalWord.reverse().join('');
+}
+
+// example
+let isPalindromeSample1 = 'race car';
+let isPalindromeSample2 = 'Madam, I\'m Adam';
+console.log( isPalindrome(isPalindromeSample1) );
+console.log( isPalindrome(isPalindromeSample2) );
+
