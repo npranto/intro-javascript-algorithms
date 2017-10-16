744de421ad8435ca41470fa6d277460ea0afa66d
Shaheen Ahmed <s.ahmed@aol.com>
2017-10-16 10:53:48 -0400

    adds reverseWords challenge

---
+++ challenges/reverseWords.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * @param str <String>
+ * @returns <String
+ */
+
+let reverseWords = (str) => {
+    return str
+        .split(' ')
+        .map((word) => {
+            return word
+                .trim()
+                .split('')
+                .reverse()
+                .join('');
+        })
+        .filter((word) => {
+            return word.length > 0;
+        })
+        .join(' ');
+}
+
+// examples
+console.log( reverseWords('this is a string of words') );   // ''
