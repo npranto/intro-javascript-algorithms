2e25f39bc2fbeecc390dc49da007f456106b5548
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-10-17 14:35:10 -0400

    adds recursion version of fibonacci

--- challenges/fibonacci.js
+++ challenges/fibonacci.js
@@ -25,9 +25,19 @@ let fibonacci = (position) => {
     return fibbs[fibbs.length - 1];
 }
 
+// simpler with recursion
+let fibonacciRecursion = (position) => {
+    if (position <= 2) {
+        return 1;
+    } else {
+        return fibonacciRecursion(position - 1) + fibonacciRecursion(position - 2);
+    }
+}
+
 // examples
 console.log( fibonacci(1) );   // 1
 console.log( fibonacci(2) );   // 1
 console.log( fibonacci(3) );   // 2
 console.log( fibonacci(4) );   // 3
 console.log( fibonacci(9) );   // 34
+console.log( fibonacciRecursion(9) );   // 34
