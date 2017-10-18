9625d991232aefee180483f92664a8348cd5c41b
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-10-18 12:31:41 -0400

    adds mergeSort algorithm

--- challenges/mergeSort.js
+++ challenges/mergeSort.js
@@ -5,9 +5,27 @@
  */
 
 let mergeSort = (arr) => {
+    if (arr.length < 2) {
+        return arr;
+    }
+    let midIndex = Math.floor(arr.length/2);
+    let left = mergeSort(arr.slice(0, midIndex));
+    let right = mergeSort(arr.slice(midIndex));
+    return merge(left, right);
+}
 
-
-
+let merge = (arr1, arr2) => {
+    let bothMerged = [];
+    while ((arr1.length !== 0) && (arr2.length !== 0)) {
+        let lesser = (arr1[0] < arr2[0]) ? arr1.shift() : arr2.shift();
+        bothMerged.push(lesser);
+    }
+    (arr1 > arr2)
+        ? bothMerged.push(...arr1)
+        : (arr2 > arr1)
+            ? bothMerged.push(...arr2)
+            : null;
+    return bothMerged;
 }
 
 // example
